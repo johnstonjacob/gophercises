3200e4b995b8b54b62dcfd99760fe18f3d9e1e9b
Jacob Johnston <johnstonajacob@gmail.com>
2018-08-21 14:45:57 -0500

    normalize strings for answer and input

--- ex/quiz_game/main.go
+++ ex/quiz_game/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/csv"
+	"strings"
 	//	"flag"
 	"fmt"
 	"os"
@@ -32,18 +33,27 @@ func getInput() string {
 	return input
 }
 
+func normalizeString(a string, i string) bool {
+	a = strings.Trim(a, " ")
+	a = strings.ToLower(a)
+
+	i = strings.Trim(i, " ")
+	i = strings.ToLower(i)
+
+	return a == i
+}
+
 func gameLoop(qa [][]string) {
 	score := 0
 
 	for _, tuple := range qa {
-		q := tuple[0]
-		a := tuple[1]
+		q, a := tuple[0], tuple[1]
 
 		fmt.Println(q)
 
 		input := getInput()
 
-		if input == a {
+		if normalizeString(a, input) {
 			score++
 			fmt.Println("Correct!")
 		} else {
