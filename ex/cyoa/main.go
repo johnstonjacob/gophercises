9e71781fd5dd74d46d534ee01f781fbc1bdc994e
Jacob Johnston <johnstonajacob@gmail.com>
2018-08-23 14:46:50 -0500

    parse json story

--- ex/cyoa/main.go
+++ ex/cyoa/main.go
@@ -1,14 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
 
 	"github.com/johnstonjacob/gophercises/ex/cyoa/parsejson"
+	"github.com/johnstonjacob/gophercises/ex/cyoa/storyhttp"
 	// "html/template"
 	// "http"
 )
 
 func main() {
-	fmt.Println(parsejson.ParseJSON("gopher.json"))
-	fmt.Println("vim-go")
+	S, err := parsejson.ParseJSON("gopher.json")
+
+	if err != nil {
+		panic(err)
+	}
+
+	storyHandler, err := storyhttp.Storyhttp(&S)
+
+	http.ListenAndServe(":8080", storyHandler)
+
 }
--- ex/cyoa/parsejson/parsejson.go
+++ ex/cyoa/parsejson/parsejson.go
@@ -2,29 +2,26 @@ package parsejson
 
 import (
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 )
 
 // ParseJSON returns a parsedJSON struct
-func ParseJSON(path string) Story {
+func ParseJSON(path string) (Story, error) {
 	var c Story
 
 	f, err := ioutil.ReadFile(path)
 
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
 	}
 
 	err = json.Unmarshal(f, &c)
 
 	if err != nil {
-		fmt.Println(err)
+		return nil, err
 	}
 
-	// fmt.Println(c["denver"])
-
-	return c
+	return c, nil
 
 }
 
